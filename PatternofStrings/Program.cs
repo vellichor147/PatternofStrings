3f1118a382d8bd3be06340e9b334c4a740d7949a
vellichor147 <71836410+vellichor147@users.noreply.github.com>
2020-12-14 21:18:10 +0100

    Add project files.

---
+++ PatternofStrings/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace PatternofStrings
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter string: ");
+            string input = Console.ReadLine();
+            int x = input.Length-1;
+
+            do
+            {
+                for (int i = 0; i < x; i++)
+                {
+                    Console.Write(input[i]);
+                }
+                Console.WriteLine();
+                x--;
+            } while (x !=0);
+            int y = x+2;
+            do
+            {
+                for (int i = 0; i < y; i++)
+                {
+                    Console.Write(input[i]);
+                }
+                Console.WriteLine();
+                y++;
+            } while (y <= input.Length);
+
+
+        }
+    }
+}
